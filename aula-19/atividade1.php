6d2d851fdf50e0208397a2dad5e4b3e6d550879d
João Paulo Polles <joaopaulopolles@gmail.com>
2023-10-26 14:07:27 -0300

    aula 19

---
+++ aula-19/atividade1.php
@@ -0,0 +1,22 @@
+<?php
+    $materias = array(
+        array("nome"=>"Banco de Dados","nota_final"=>rand(7,10)),
+        array("nome"=>"Análise de Sistemas","nota_final"=>rand(7,10)),
+        array("nome"=>"Ciência da Computação","nota_final"=>rand(7,10)),
+        array("nome"=>"Frontend","nota_final"=>rand(7,10)),
+        array("nome"=>"Programação Mobile","nota_final"=>rand(7,10)),
+        array("nome"=>"Português","nota_final"=>rand(7,10)),
+        array("nome"=>"Matemática","nota_final"=>rand(7,10)),
+        array("nome"=>"História","nota_final"=>rand(7,10)),
+        array("nome"=>"Geografia","nota_final"=>rand(7,10)),
+        array("nome"=>"Química","nota_final"=>rand(7,10)),
+        array("nome"=>"Biologia","nota_final"=>rand(7,10)),
+        array("nome"=>"Sociologia","nota_final"=>rand(7,10)),
+        array("nome"=>"Projeto de Vida","nota_final"=>rand(7,10)),
+        array("nome"=>"Educação Financeira","nota_final"=>rand(7,10)),
+    );
+
+    foreach($materias as $materia){
+        echo $materia["nome"]." - ".$materia["nota_final"]."<br/>";
+    }
+?>
